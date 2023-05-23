4af8da870831ecf246335cf66d181d09062cf556
FernEscDev <hoopa8645@gmail.com>
2023-05-23 11:46:28 -0300

    a

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,19 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="es">
-      <Head/>
+      <Head>
+        <title>'Vaya, Valla y Baya'</title>
+        <meta
+          name="description"
+          content="Exposicion sobre 'Vaya', 'Valla' y 'Baya'"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link
+          rel="icon"
+          type="image/x-icon"
+          href="https://cdn-icons-png.flaticon.com/128/3137/3137134.png"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,5 @@
 import Page1 from "@/components/Page1";
-import Head from "next/head";
 
 export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>'Vayan, valla'</title>
-        <meta name="description" content="Portafolio personal de FernDev." />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
-      <Page1/>
-    </>
-  );
+  return <Page1 />;
 }
