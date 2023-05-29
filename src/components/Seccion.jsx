a31bf96be30883f419000bd4453734f570117406
FernEscDev <hoopa8645@gmail.com>
2023-05-29 11:41:10 -0300

    a

--- src/components/Seccion.jsx
+++ src/components/Seccion.jsx
@@ -33,11 +33,11 @@ const Seccion = () => {
           w={"35%"}
           h={"xl"}
           as="h1"
-          fontSize={{base: '4xl',md:'5xl',lg:'6xl'}}
+          fontSize={{md:'5xl',lg:'6xl'}}
         >
           Uso del "Vaya", "Valla" y "Baya"
         </Heading>
-        <Heading as="h3" pb={"14"} fontSize={{base: 'md',md:'lg',lg:'xl'}}>
+        <Heading as="h3" pb={"14"} fontSize={{md:'lg',lg:'xl'}}>
           Comprensión y producción de textos
         </Heading>
       </Flex>
